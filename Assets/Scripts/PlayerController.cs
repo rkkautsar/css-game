4b8e6a5d04aec71857bbd96dda17c1823a778597
Rakha Kanz Kautsar <rkkautsar@gmail.com>
2017-10-21 16:10:28 +0700

    player movement (arrow key)

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveSpeed = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Mathf.Abs(Input.GetAxisRaw ("Horizontal")) > 0) {
+			Vector3 vector = new Vector3 (Input.GetAxisRaw ("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f);
+			transform.Translate (vector);
+		}
+
+		if (Mathf.Abs(Input.GetAxisRaw ("Vertical")) > 0) {
+			Vector3 vector = new Vector3 (0f, Input.GetAxisRaw ("Vertical") * moveSpeed * Time.deltaTime, 0f);
+			transform.Translate (vector);
+		}
+	}
+}
