dd33f7256aabcd3bda42d9a5e9780a1ceeb67256
Rakha Kanz Kautsar <rkkautsar@gmail.com>
2017-10-21 21:41:39 +0700

    add animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
 	public float moveSpeed = 5.0f;
+	private Animator anim;
 
 	// Use this for initialization
 	void Start () {
-		
+		anim = GetComponent<Animator> ();
+
 	}
 	
 	// Update is called once per frame
@@ -22,5 +24,8 @@ public class PlayerController : MonoBehaviour {
 			Vector3 vector = new Vector3 (0f, Input.GetAxisRaw ("Vertical") * moveSpeed * Time.deltaTime, 0f);
 			transform.Translate (vector);
 		}
+
+		anim.SetFloat ("MoveX", Input.GetAxisRaw ("Horizontal"));
+		anim.SetFloat ("MoveY", Input.GetAxisRaw ("Vertical"));
 	}
 }
