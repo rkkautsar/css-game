8d612dc4cb7a4a4f6466dd45f68cb7de76e223be
Tri Ahmad Irfan <triahmadirfan@gmail.com>
2017-10-23 10:49:42 +0700

    Add simple game Timer

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,20 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
 	public float moveSpeed = 5.0f;
+	public Text timerText;
 	private Animator anim;
 
 	// Use this for initialization
 	void Start () {
 		anim = GetComponent<Animator> ();
-
+		timerText.text = Time.time.ToString();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		timerText.text = Time.time.ToString();
+
 		if (Mathf.Abs(Input.GetAxisRaw ("Horizontal")) > 0) {
 			Vector3 vector = new Vector3 (Input.GetAxisRaw ("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f);
 			transform.Translate (vector);
