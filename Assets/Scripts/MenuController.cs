fd07341d18f377fa2d794f8903b1ac940a7d7f6f
anabmaulana <maulanaanab@gmail.com>
2017-10-28 21:55:00 +0700

    finish create pause and quit menu

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour {
+
+    public Transform canvas;
+    public Transform Player;
+    public Button exitButton;
+
+    private void Start()
+    {
+        exitButton.onClick.AddListener(exitOnClick);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (canvas.gameObject.activeInHierarchy == false)
+            {
+                canvas.gameObject.SetActive(true);
+                Time.timeScale = 0;
+                Player.GetComponent<Animator>().enabled = false;
+            } else
+            {
+                canvas.gameObject.SetActive(false);
+                Time.timeScale = 1;
+                Player.GetComponent<Animator>().enabled = true;
+            }
+        }
+	}
+
+    void exitOnClick()
+    {
+        Application.Quit();
+    }
+}
