e188327b3d6c23d4925fa1d1645233d80683a21e
Rakha Kanz Kautsar <rkkautsar@gmail.com>
2017-10-29 00:33:48 +0700

    add char movement with mouse

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -24,45 +24,38 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-		if (Mathf.Abs(Input.GetAxisRaw ("Horizontal")) > 0) {
-			Vector3 vector = new Vector3 (Input.GetAxisRaw ("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f);
-			transform.Translate (vector);
-
-			anim.SetFloat ("MoveX", Input.GetAxisRaw ("Horizontal"));
-			anim.SetFloat ("MoveY", Input.GetAxisRaw ("Vertical"));
-			isMouseMoving = false;
-		}
-
-		if (Mathf.Abs(Input.GetAxisRaw ("Vertical")) > 0) {
-			Vector3 vector = new Vector3 (0f, Input.GetAxisRaw ("Vertical") * moveSpeed * Time.deltaTime, 0f);
-			transform.Translate (vector);
-
-			anim.SetFloat ("MoveX", Input.GetAxisRaw ("Horizontal"));
-			anim.SetFloat ("MoveY", Input.GetAxisRaw ("Vertical"));
-			isMouseMoving = false;
-		}
-
 		if (Input.GetMouseButtonUp (0)) {
-			destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			destination = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 			destination.z = 0;
 			isMouseMoving = true;
 		}
 
+		if (!isMouseMoving) {
+			if (Mathf.Abs(Input.GetAxisRaw ("Horizontal")) > 0) {
+				Vector3 vector = new Vector3 (Input.GetAxisRaw ("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f);
+				transform.Translate (vector);
+			}
 
-		if (isMouseMoving) {
+			if (Mathf.Abs(Input.GetAxisRaw ("Vertical")) > 0) {
+				Vector3 vector = new Vector3 (0f, Input.GetAxisRaw ("Vertical") * moveSpeed * Time.deltaTime, 0f);
+				transform.Translate (vector);
+			}
+
+			anim.SetFloat ("MoveX", Input.GetAxisRaw ("Horizontal"));
+			anim.SetFloat ("MoveY", Input.GetAxisRaw ("Vertical"));
+	
+		} else {
 			Vector3 source = rt.position;
 			Vector3 delta = destination - source;
 
 
-
-			mouseText.text = delta.ToString ();
-
-			if (Mathf.Abs (delta.x) > 0) {
+		
+			if (Mathf.Abs (delta.x) > 1) {
 				Vector3 vector = new Vector3 (delta.x * moveSpeed * Time.deltaTime, 0f, 0f);
 				transform.Translate (vector);
 				anim.SetFloat ("MoveX", delta.x);
 				anim.SetFloat ("MoveY", 0f);
-			} else if (Mathf.Abs (delta.y) > 0) {
+			} else if (Mathf.Abs (delta.y) > 1) {
 				Vector3 vector = new Vector3 (0f, delta.y * moveSpeed * Time.deltaTime, 0f);
 				transform.Translate (vector);
 				anim.SetFloat ("MoveX", 0f);
