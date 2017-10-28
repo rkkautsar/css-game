ab2a51f4efd65b5afb5e774032583dc177824883
Rakha Kanz Kautsar <rkkautsar@gmail.com>
2017-10-29 00:16:50 +0700

    wip player mouse moving

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public Text timerText;
+
+	// Use this for initialization
+	void Start () {
+		timerText.text = Time.time.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timerText.text = Time.time.ToString();
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,31 +5,74 @@ using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
+	public Text mouseText;
+
 	public float moveSpeed = 5.0f;
-	public Text timerText;
 	private Animator anim;
+	private RectTransform rt;
+	public Sprite player;
+	private Vector3 destination;
+	private bool isMouseMoving;
 
 	// Use this for initialization
 	void Start () {
 		anim = GetComponent<Animator> ();
-		timerText.text = Time.time.ToString();
+		rt = GetComponent<RectTransform> ();
+		isMouseMoving = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		timerText.text = Time.time.ToString();
 
 		if (Mathf.Abs(Input.GetAxisRaw ("Horizontal")) > 0) {
 			Vector3 vector = new Vector3 (Input.GetAxisRaw ("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f);
 			transform.Translate (vector);
+
+			anim.SetFloat ("MoveX", Input.GetAxisRaw ("Horizontal"));
+			anim.SetFloat ("MoveY", Input.GetAxisRaw ("Vertical"));
+			isMouseMoving = false;
 		}
 
 		if (Mathf.Abs(Input.GetAxisRaw ("Vertical")) > 0) {
 			Vector3 vector = new Vector3 (0f, Input.GetAxisRaw ("Vertical") * moveSpeed * Time.deltaTime, 0f);
 			transform.Translate (vector);
+
+			anim.SetFloat ("MoveX", Input.GetAxisRaw ("Horizontal"));
+			anim.SetFloat ("MoveY", Input.GetAxisRaw ("Vertical"));
+			isMouseMoving = false;
 		}
 
-		anim.SetFloat ("MoveX", Input.GetAxisRaw ("Horizontal"));
-		anim.SetFloat ("MoveY", Input.GetAxisRaw ("Vertical"));
+		if (Input.GetMouseButtonUp (0)) {
+			destination = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			destination.z = 0;
+			isMouseMoving = true;
+		}
+
+
+		if (isMouseMoving) {
+			Vector3 source = rt.position;
+			Vector3 delta = destination - source;
+
+
+
+			mouseText.text = delta.ToString ();
+
+			if (Mathf.Abs (delta.x) > 0) {
+				Vector3 vector = new Vector3 (delta.x * moveSpeed * Time.deltaTime, 0f, 0f);
+				transform.Translate (vector);
+				anim.SetFloat ("MoveX", delta.x);
+				anim.SetFloat ("MoveY", 0f);
+			} else if (Mathf.Abs (delta.y) > 0) {
+				Vector3 vector = new Vector3 (0f, delta.y * moveSpeed * Time.deltaTime, 0f);
+				transform.Translate (vector);
+				anim.SetFloat ("MoveX", 0f);
+				anim.SetFloat ("MoveY", delta.y);
+			} else {
+				isMouseMoving = false;
+				anim.SetFloat ("MoveX", 0f);
+				anim.SetFloat ("MoveY", 0f);
+			}
+		}
+
 	}
 }
