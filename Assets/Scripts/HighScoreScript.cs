02388f09b02d92fc0573271cdbe2d1ba2608d00c
maulanaanab <maulanaanab@gmail.com>
2017-12-03 16:36:29 +0700

    implement highscore script (untested)

---
+++ Assets/Scripts/HighScoreScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighScoreController : MonoBehaviour {
+
+    const string prefKey = "highscore_";
+    const int numOfScores = 10;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 1; i <= numOfScores; i++)
+        {
+            if (PlayerPrefs.HasKey(getPrefKey(i)))
+            {
+                PlayerPrefs.SetString(getPrefKey(i), "COMP 0.0");
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    string getPrefKey(int index)
+    {
+        return prefKey + index;
+    }
+
+    public void updateHighScore(string name, float score)
+    {
+        string newHighScore = name + " " + score;
+        for (int i = 1; i <= numOfScores; i++)
+        {
+            string currentHighScore = PlayerPrefs.GetString(getPrefKey(i));
+
+            float newScore = float.Parse(newHighScore.Split(' ')[1]);
+            float currentScore = float.Parse(currentHighScore.Split(' ')[1]);
+
+            if (newScore - currentScore > 1e-9)
+            {
+                PlayerPrefs.SetString(getPrefKey(i), newHighScore);
+
+                // to update next high score
+                newHighScore = currentHighScore;
+            }
+        }
+        PlayerPrefs.Save();
+    }
+}
